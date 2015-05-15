d1b3ff037d46d44e6c3948b9f3c1221466ea8702
Chyld Medford <chyld.medford@gmail.com>
2015-05-15 13:41:12 -0700

    plague

---
+++ client/directives/cm-plague/cm-plague.js
@@ -0,0 +1,47 @@
+'use strict';
+
+angular.module('cmPlagueModule', [])
+.directive('cmPlague', function(){
+  var o = {};
+
+  o.restrict = 'A';
+  o.templateUrl = '/directives/cm-plague/cm-plague.html';
+  o.scope = {};
+  // o.link = function($scope, element, attrs){};
+  o.controller = function($interval, $http, $window, $scope){
+    function getPlague(){
+      var url = 'https://qrng.anu.edu.au/API/jsonI.php?length=1&type=uint16';
+      $window.jQuery.get(url, function(response){
+        var random = response.data[0];
+        var plagueId = random % 4;
+        var plague;
+        var damage;
+
+        switch(plagueId){
+          case 0:
+            plague = 'fire';
+            damage = random % 40;
+            break;
+          case 1:
+            plague = 'bees';
+            damage = random % 20;
+            break;
+          case 2:
+            plague = 'termites';
+            damage = random % 30;
+            break;
+          case 3:
+            plague = 'brisk wind';
+            damage = random % 10;
+        }
+
+        $scope.plague = plague;
+        $scope.damage = damage;
+      });
+    }
+
+    $interval(getPlague, 3000);
+  };
+
+  return o;
+});
--- client/index.js
+++ client/index.js
@@ -1,3 +1,3 @@
 'use strict';
 
-angular.module('trees', ['firebase', 'ui.router', 'cmTreeModule']);
+angular.module('trees', ['firebase', 'ui.router', 'cmTreeModule', 'cmPlagueModule']);
